d9f0c64544c1ae169c8c6942d59687ec1a545fed
Junix31 <junioraquino893@gmail.com>
2024-11-02 13:32:37 -0400

    ola

---
+++ Arreglos/02-Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Channels;
+using System.Xml.Linq;
+
+namespace _02_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //una tienda electronica tiene una matrices que representa e
+            //l inventario de distintos tipos de dispositivos(televisores,
+            //laptops, telefonos, etc)en varios almacenes.cada fila representa
+            //un tipo de dispositivo y cada columna un almacen
+
+            //calcula la cantidad total de dispositivos en cada almacen
+
+            //determina que dispositivo tiene mayor y menor cantidad en cada almacen.
+
+            int[,] inventario = new int[,] { { }, { }, { },  };
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Arreglos/arreglosEnelcurso/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace arreglosEnelcurso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+        }
+
+        public void Estudiantes(int[] calificaciones, string[] nombres) 
+        {
+            Console.WriteLine($"el nombre del estudiantes es: {nombres} y la calificacion es: {calificaciones}");
+        }
+    }
+}
